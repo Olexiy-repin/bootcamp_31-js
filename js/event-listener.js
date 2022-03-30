5902518b17dc2f61ca87961e8e13ce4c6a5ad07c
Olexiy-repin <31596886+Olexiy-repin@users.noreply.github.com>
2022-03-30 12:30:05 +0300

    classwork

--- js/event-listener.js
+++ js/event-listener.js
@@ -11,3 +11,16 @@
  */
 
 // https://picsum.photos/200/300
+const imgEl = document.querySelector('.js-img');
+const swapBtnEl = document.querySelector('.js-swap-btn');
+const removeListenerBtnEl = document.querySelector('.js-remove-listener');
+
+const onSwapBtnClick = function () {
+  imgEl.src = 'https://picsum.photos/200/300';
+  imgEl.alt = 'random image';
+};
+
+swapBtnEl.addEventListener('click', onSwapBtnClick);
+removeListenerBtnEl.addEventListener('click', () => {
+  swapBtnEl.removeEventListener('click', onSwapBtnClick);
+});
--- js/keyboard-events.js
+++ js/keyboard-events.js
@@ -2,3 +2,9 @@
 /*
  * События клавиатуры. keypress, keydown, keyup
  */
+
+const outputEl = document.querySelector('.js-output');
+
+document.addEventListener('keyup', event => {
+  outputEl.insertAdjacentHTML('beforeend', `code: ${event.code} <br> key: ${event.key} <br> <hr>`);
+});
